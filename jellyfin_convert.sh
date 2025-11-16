8986238a02a027aa3c566eb1e283e544200f87e3
bpr <boguslaw.proszkowiec@gmail.com>
2025-11-16 08:06:51 +0100

    Modify transcoding parameters based on jellyfin log.

--- jellyfin_convert.sh
+++ jellyfin_convert.sh
@@ -2,12 +2,16 @@
 
 VIDEO_CODEC="libx264"       # Video codec to use: libx264 (H.264), libx265 (H.265/HEVC), etc.
 PRESET="slow"               # Encoding speed to compression ratio: ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
+PROFILE="high"              # H.264 profile: baseline, main, high
+PIX_FMT="yuv420p"           # Pixel format
+LEVEL="41"                  # H.264 level: 3.0, 3.1, 4.0, 4.1, 4.2, 5.0, etc.
 CRF=23                      # Constant Rate Factor: lower is better quality (range 0-51)
 CA="aac"                    # Audio codec to use: AAC, AC3, etc.
+AC="2"                      # Number of audio channels
 BA="192k"                   # Audio bitrate
 
 
-FFMPEG_OPTS="-c:v $VIDEO_CODEC -preset $PRESET -crf $CRF -c:a $CA -b:a $BA -movflags +faststart"
+FFMPEG_OPTS="-codec:v:0 $VIDEO_CODEC -preset $PRESET -profile:v:0 $PROFILE -level $LEVEL -crf $CRF -c:a $CA -ac $AC -b:a $BA -pix_fmt $PIX_FMT -movflags +faststart"
 
 function usage  {
 	echo "Usage: ${0} [-r]" >&2
